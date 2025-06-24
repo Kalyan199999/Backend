fd4c0ad51e2cc925a58a4797abafd34e8d8addc7
kalyan199999 <gottapupavankalyan@gmail.com>
2025-06-24 12:00:31 +0530

    stripe payment is done

--- mongoose_db/controllers/paymentController.js
+++ mongoose_db/controllers/paymentController.js
@@ -1,27 +1,32 @@
+// Load the Stripe library
 const Stripe = require("stripe");
 
+// Get the Stripe secret key
 const STRIPE_SECRET_KEY = require('./myKey')
 
+// Use the key
 const stripe = Stripe(STRIPE_SECRET_KEY);
 
-// const dotenv = require("dotenv");
-
-// dotenv.config();
 const makePayment =  async (req, res) => {
-
-        const { amount, currency } = req.body;
-
+  
         try 
         {
+          const { amount, currency } = req.body;
+
           const paymentIntent = await stripe.paymentIntents.create({
+
             amount: amount,
             currency: currency,
             payment_method_types: ["card"],
+
           });
       
           res.status(200).json({
+
             clientSecret: paymentIntent.client_secret,
+            
             message:"Payment Intent created successfully!"
+
           });
         } 
         catch (error) 
